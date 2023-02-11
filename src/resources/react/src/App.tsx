928b18fb0adf274d33cfde231d3ed070702239c1
kairi <rmuraoka0217@gmail.com>
2023-02-12 02:41:30 +0900

    frontend

--- src/resources/react/src/App.tsx
+++ src/resources/react/src/App.tsx
@@ -1,34 +1,155 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import {
+    Box, Button,
+    Card,
+    CardContent,
+    CardHeader,
+    Fab,
+    Fade,
+    IconButton,
+    Menu,
+    MenuItem,
+    Modal,
+    Typography
+} from "@mui/material";
+import TextField from '@mui/material/TextField';
+import React from "react";
+import {AccountCircle} from "@mui/icons-material";
+import AddIcon from '@mui/icons-material/Add';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+type noteItem = {
+    title: Date;
+    content: string;
+    translation: string;
+};
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    borderRadius: '4px',
+    boxShadow: 24,
+    p: 4,
+    outline: 0,
+};
 
 function App() {
-    const [count, setCount] = useState(0)
+    const notes: noteItem[] = [
+        {title: new Date(), content: "do somethingあああああああああああああああああああああああああああああああ", translation: "翻訳されたのがこちら"},
+        {title: new Date(), content: "英語間違ってた", translation: "純粋につらい"},
+    ];
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const menuOpen = Boolean(anchorEl);
+    const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            {notes.map((note: noteItem) => {
+                return (
+                    <Card sx={{maxWidth: 500, mt: 3}}>
+                        <CardHeader
+                            avatar={
+                                <AccountCircle fontSize="large"/>
+                            }
+                            title={note.title.toDateString()}
+                            action={
+                                <IconButton
+                                    aria-label="settings"
+                                    id="fade-button"
+                                    aria-controls={menuOpen ? 'fade-menu' : undefined}
+                                    aria-haspopup="true"
+                                    aria-expanded={menuOpen ? 'true' : undefined}
+                                    onClick={handleMenuClick}
+                                >
+                                    <MoreVertIcon/>
+                                </IconButton>
+                            }
+                        />
+                        <Menu
+                            id="fade-menu"
+                            MenuListProps={{
+                                'aria-labelledby': 'fade-button',
+                            }}
+                            anchorEl={anchorEl}
+                            open={menuOpen}
+                            onClose={handleMenuClose}
+                            TransitionComponent={Fade}
+                        >
+                            <MenuItem onClick={handleMenuClose}>Update</MenuItem>
+                            <MenuItem onClick={handleMenuClose}>Delete</MenuItem>
+                        </Menu>
+                        <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                                {note.content}
+                            </Typography>
+                        </CardContent>
+                        <CardContent>
+                            {/*<CircularProgress/>*/}
+                            <Typography paragraph>
+                                {note.translation}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                );
+            })}
+            <Fab
+                color="primary"
+                aria-label="add"
+                sx={{
+                    position: 'absolute',
+                    bottom: 16,
+                    right: 16,
+                }}
+                onClick={handleOpen}
+            >
+                <AddIcon/>
+            </Fab>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <TextField
+                        fullWidth={true}
+                        id="filled-multiline-static"
+                        label="note"
+                        multiline
+                        rows={4}
+                        variant="standard"
+                    />
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'row-reverse'
+                    }}>
+                        <Button
+                            sx={{
+                                mt: 3,
+                            }}
+                            variant="contained"
+                        >
+                            send
+                        </Button>
+                    </Box>
+                </Box>
+            </Modal>
         </div>
-    )
+    );
 }
 
 export default App
